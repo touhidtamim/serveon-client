a10f8063036d35543f741744c8916779f9c8ca57
Touhid Tamim <touhidtamim1@gmail.com>
2026-01-10 21:11:30 +0600

    created basic file structure

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router/dom";
-import router from "./Router/PublicRoute.jsx";
+import router from "./routes/PublicRoutes";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
